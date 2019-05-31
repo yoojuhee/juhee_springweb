9260ecdbe7253cfd3b383bc3b4f1cd4f4f8c7090
유주희 <yoojh96@naver.com>
2019-05-31 17:44:24 +0900

    소스 및 폴더 추가

--- src/main/java/org/juhee/letter/LetterController.java
+++ src/main/java/org/juhee/letter/LetterController.java
@@ -1,5 +1,17 @@
 package org.juhee.letter;
 
+import org.juhee.book.chap11.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
 public class LetterController {
 
+	@GetMapping("/letter/listOfReceiver")
+	public void listOfReceiver(@SessionAttribute("MEMBER") Member member
+			Model model) {
+		
+	}
+	
 }
